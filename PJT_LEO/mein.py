7dd730e8f7a7875f8eb84d1b9006fad9aecd8769
Pedro Henrique <pedro.henriquesil@hotmail.com>
2024-10-22 15:59:14 -0300

    pjt leo

--- PJT_LEO/mein.py
+++ PJT_LEO/mein.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+from psycopg2 import sql
 
 app = Flask(__name__)
 
+def connect_db():
+    conn = psycopg2.connect(
+        dbname="marchi_arquitetos", 
+        user="PHS", 
+        password="0509", 
+        host="localhost", 
+        port="5432"
+    )
+    return conn
+
+conn = connect_db()
+cur = conn.cursor()
 
 @app.route('/')
 def index():
@@ -9,9 +23,44 @@ def index():
 
 @app.route('/orcamento', methods=['GET', 'POST'])
 def orcamento():
-      return render_template('orc_index.html')
+      if request.method == 'POST':
+            tipo     = request.form['G01']
+            ambiente = request.form['G02']
+            quartos  = request.form['G03']
+            garagem  = request.form['G04']
+            list_ambientes = request.form.getlist('G05')
+
+            goumet = hometheater = despejo = escritorio = academia = piscina = False
+
+            for i in list_ambientes:
+                  if i == 'area_goumet':
+                        goumet = True
+                  elif i == 'hometheaterr':
+                        hometheater = True
+                  elif i == 'despejo':
+                        despejo = True
+                  elif i == 'escritorio':
+                        escritorio = True
+                  elif i == 'academia':
+                        academia = True
+                  elif i == 'piscina':
+                        piscina = True
+            try:
+                  query = sql.SQL("INSERT INTO orcamento (orc_tipo, orc_ambiente, orc_quartos,\
+                                    orc_garagem, orc_goumet, orc_hometheater, orc_despejo, orc_escritorio,\
+                                    orc_academia, orc_piscina) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+                        
+                  cur.execute(query, (tipo, ambiente,quartos,garagem,goumet,hometheater,despejo,escritorio,academia,piscina))
+                  conn.commit()
+                
+            except Exception as e:
+                  conn.rollback() 
+                  print(f"Erro ao inserir dados: {e}")
+
+            return redirect(url_for('index.html'))
+return render_template('orc_index.html')
 
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=5000,debug=True)
+      app.run(host='0.0.0.0', port=5000,debug=True)
 
